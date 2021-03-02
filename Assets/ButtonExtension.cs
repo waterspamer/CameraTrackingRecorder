154d1279bea512a4be556433de3565a77b4c80c2
waterspamer <48622741+waterspamer@users.noreply.github.com>
2021-03-02 20:58:36 +0300

    upd
    started UI-works

---
+++ Assets/ButtonExtension.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class ButtonExtension : Button
+{
+
+    [SerializeField] public GameObject circle;
+    // Start is called before the first frame update
+    public override void OnPointerDown(PointerEventData eventData)
+    {
+        base.OnPointerDown(eventData);
+       // var circleRect = circle.rect;
+        //circleRect.size *= 2;
+        //circle.transform.localScale *= 2;
+        circle.GetComponent<RectTransform>().position = eventData.position;
+        StartCoroutine(scalingRoutine);
+    }
+
+    private IEnumerator scalingRoutine;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        scalingRoutine = CircleScaling();
+    }
+
+    public override void OnPointerUp(PointerEventData eventData)
+    {
+        base.OnPointerUp(eventData);
+        StopCoroutine(scalingRoutine);
+        circle.transform.localScale = Vector3.one * .01f;
+        GetComponent<Animator>().enabled = true;
+    }
+
+
+    IEnumerator CircleScaling()
+    {
+        while (true)
+        {
+            circle.transform.localScale *= 1.11f;
+            yield return new WaitForFixedUpdate();
+        }
+        
+    }
+}
---
+++ Assets/Scripts/Editor/ButtonExtensionEditor.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+
+[CustomEditor(typeof(ButtonExtension))]
+public class ButtonExtensionEditor : UnityEditor.UI.ButtonEditor
+{
+    public override void OnInspectorGUI()
+    {
+        
+        var component = (ButtonExtension)target;
+        
+        base.OnInspectorGUI();
+
+        component.circle = (GameObject)EditorGUILayout.ObjectField("Circle Game Object", 
+            component.circle, typeof(GameObject), true);
+
+    }
+}
