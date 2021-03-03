e57fb99a898b92f6c284e93ada726d85e32f2af1
waterspamer <48622741+waterspamer@users.noreply.github.com>
2021-03-03 20:57:21 +0300

    upd
    UI update

---
+++ Assets/Scripts/UI/UIElementScaler.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIElementScaler : MonoBehaviour
+{
+    
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale = Vector3.one * (1 - Mathf.Abs(GetComponent<RectTransform>().position.x - Screen.width / 2f) / 3000f);
+    }
+}
